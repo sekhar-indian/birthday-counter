09cf6144596aa01d818d0a3411d791accb53d948
Munisekhar Udavalapati <124375666+sekhar-indian@users.noreply.github.com>
2024-11-20 18:17:17 +0530

    Update index.js

--- scripts/index.js
+++ scripts/index.js
@@ -4,7 +4,7 @@ const giftbox = document.getElementById('merrywrap');
 const canvasC = document.getElementById('c');
 
 const config = {
-  birthdate: 'Jan 29, 2020',
+  birthdate: 'Nov 21, 2024',
   name: 'Darlene'
 };
 
